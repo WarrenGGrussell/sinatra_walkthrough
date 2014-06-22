d75e7996e8e42341db87d1e69a86e2a5811cfb72
WarrenG <warrencoke@gmail.com>
2014-06-22 20:41:55 +0000

    Part 4 done: took a while for me to trouble-shoot my index.rb when it should've been .erb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140622201000) do
+ActiveRecord::Schema.define(version: 20140622203312) do
 
   create_table "messages", force: true do |t|
     t.string "title"
